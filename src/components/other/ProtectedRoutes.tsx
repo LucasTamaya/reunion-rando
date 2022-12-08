8247572524d5feae5c82f8f9b23d74f512683a46
LucasTamaya <lucastamaya.974@gmail.com>
2022-12-08 16:31:21 +0400

    updated Unauthorized Screen

--- src/components/other/ProtectedRoutes.tsx
+++ src/components/other/ProtectedRoutes.tsx
@@ -1,5 +1,7 @@
-import { useUserRole } from "@/hooks/user/useUserRole";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
+
+import { useUserRole } from "@/hooks/user/useUserRole";
 
 type AllowedRoles = "particulier" | "prestataire";
 
@@ -7,11 +9,14 @@ export const ProtectedRoutes: React.FC<{ allowedRole: AllowedRoles }> = ({
   allowedRole,
 }) => {
   const { isLoading, data: user } = useUserRole();
-
   const location = useLocation();
 
   if (isLoading) {
-    return <p>Loading...</p>;
+    return (
+      <div className="w-full h-[80vh] flex justify-center items-center">
+        <ClipLoader size={25} speedMultiplier={0.9} color="#128B2D" />
+      </div>
+    );
   }
 
   return user?.role === allowedRole ? (
--- src/screens/Unauthorized.tsx
+++ src/screens/Unauthorized.tsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const Unauthorized: React.FC = () => {
-  return <h1>Vous ne disposez pas des droit nécessaire!</h1>;
+  return (
+    <div className="h-[80vh] max-w-3xl flex flex-col justify-center items-center mx-auto px-5">
+      <h1 className="text-2xl sm:text-6xl text-main-green text-center font-semibold my-10 sm:my-16">
+        Vous ne disposez pas des droits nécessaires
+      </h1>
+      <button className="w-full text-white text-base sm:text-lg font-semibold bg-main-green rounded p-2 sm:p-3">
+        <Link className="block w-full h-full" to="/">
+          Revenir à l'accueil
+        </Link>
+      </button>
+    </div>
+  );
 };
