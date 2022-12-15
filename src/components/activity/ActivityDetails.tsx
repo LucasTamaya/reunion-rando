641d80ec527bfa4fbd50590feb35cfa6b47ddda0
LucasTamaya <lucastamaya.974@gmail.com>
2022-12-15 16:52:19 +0400

    added unit test for ActivityDetails component

--- src/components/activity/ActivityDetails.tsx
+++ src/components/activity/ActivityDetails.tsx
@@ -1,54 +1,65 @@
 import { useLocation } from "react-router-dom";
+import { BsPersonCircle } from "react-icons/bs";
 
 import { Nav } from "../common/nav/Nav";
 import { Button } from "../common/Button";
 import { Activity } from "@/types";
-import { BsPersonCircle } from "react-icons/bs";
 
 export const ActivityDetails: React.FC = () => {
-  // Retrieve the activity details from the location state
+  // Retrieve and destructure the activity details from the location state
   const location = useLocation();
-  const activity: Activity = location.state;
-  const AVATAR_URL = activity.createdBy.avatar
-    ? `url("${activity.createdBy.avatar}")`
-    : "";
+  const {
+    title,
+    image_url,
+    description,
+    price,
+    createdBy,
+    location: hikeLocation,
+  }: Activity = location.state;
+  const { lastname, firstname, email, avatar } = createdBy;
+  const AVATAR_URL = avatar ? `url("${avatar}")` : "";
 
   return (
     <>
       <Nav />
       <h1 className="text-main-green text-2xl sm:text-6xl text-center font-semibold my-10 sm:my-16">
-        {activity.title}
+        {title}
       </h1>
       <div className="max-w-7xl grid grid-cols-1 md:grid-cols-2 gap-x-10 mx-auto px-5 pb-5">
-        <img src={activity.image_url} alt="randonnée" className="rounded" />
+        <img src={image_url} alt="randonnée" className="rounded" />
         <div>
           <h2 className="text-xl sm:text-3xl text-black font-semibold mt-5 md:m-0">
             Description
           </h2>
-          <p className="text-base sm:text-lg mb-10">{activity.description}</p>
+          <p className="text-base sm:text-lg mb-10">{description}</p>
           <h2 className="text-xl sm:text-3xl text-black font-semibold mb-3">
             Proposé par
           </h2>
           <div className="flex items-start mb-10">
             <p className="text-base sm:text-lg mr-5">
-              {activity.createdBy.lastname} {activity.createdBy.firstname}
+              {lastname} {firstname}
             </p>
-            {activity.createdBy.avatar ? (
+            {avatar ? (
               <div
                 className="w-24 h-24 rounded-full bg-cover"
                 style={{ backgroundImage: AVATAR_URL }}
+                data-testid="avatar"
               ></div>
             ) : (
-              <BsPersonCircle size={96} color="grey" />
+              <BsPersonCircle
+                size={96}
+                color="grey"
+                data-testid="emptyAvatarIcon"
+              />
             )}
           </div>
           <h2 className="text-xl sm:text-3xl text-black font-semibold">Prix</h2>
-          <p className="text-base sm:text-lg mb-10">{activity.price} &euro;</p>
+          <p className="text-base sm:text-lg mb-10">{price} &euro;</p>
           <h2 className="text-xl sm:text-3xl text-black font-semibold">
             Localisation
           </h2>
-          <p className="text-base sm:text-lg mb-10">{activity.location}</p>
-          <a href={`mailto:${activity.createdBy.email}`} className="w-full">
+          <p className="text-base sm:text-lg mb-10">{hikeLocation}</p>
+          <a href={`mailto:${email}`} className="w-full">
             <Button text="Contacter le prestataire" variant="primary" />
           </a>
         </div>
---
+++ tests/components/activity/ActivityDetails.test.tsx
@@ -0,0 +1,107 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import * as ReactRouter from "react-router-dom";
+
+import { ActivityDetails } from "@/components/activity/ActivityDetails";
+import { Activity } from "@/types";
+
+vi.mock("react-router-dom", () => ({
+  useLocation: vi.fn(),
+  useNavigate: vi.fn(),
+}));
+
+describe("ActivityDetails Component", () => {
+  it("should renders the component correctly with a user avatar", () => {
+    // Mock location state with activityDetails data and a user avatar
+    vi.spyOn(ReactRouter, "useLocation").mockReturnValue({
+      state: {
+        title: "Randonnée à Mafate",
+        image_url: "mafate.jpg",
+        description: "Superbe randonnée à Mafate",
+        price: 40,
+        location: "Mafate",
+        createdBy: {
+          lastname: "Doe",
+          firstname: "John",
+          email: "john.doe@gmail.com",
+          avatar: "avatar.jpg",
+        },
+        cloudinary_public_id: "",
+        id: "",
+        userId: "",
+      } as Activity,
+      pathname: "",
+      key: "key",
+      hash: "",
+      search: "",
+    });
+
+    render(<ActivityDetails />);
+
+    const title = screen.getByRole("heading", { name: /randonnée à mafate/i });
+    const hikeImage = screen.getByAltText(/randonnée/i);
+    const description = screen.getByText(/superbe randonnée à mafate/i);
+    const userFullName = screen.getByText(/doe john/i);
+    const avatar = screen.getByTestId("avatar");
+    const price = screen.getByText(/40 €/i);
+    const location = screen.getByText("Mafate");
+    const mailToLink = screen.getByRole("link");
+
+    expect(title).toBeInTheDocument();
+    expect(hikeImage).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(userFullName).toBeInTheDocument();
+    expect(avatar).toBeInTheDocument();
+    expect(price).toBeInTheDocument();
+    expect(location).toBeInTheDocument();
+    expect(mailToLink).toBeInTheDocument();
+    expect(mailToLink).toHaveAttribute("href", "mailto:john.doe@gmail.com");
+  });
+
+  it("should renders the component correctly with a user avatar", () => {
+    // Mock location state with activityDetails data and an empty user avatar
+    vi.spyOn(ReactRouter, "useLocation").mockReturnValue({
+      state: {
+        title: "Randonnée à Mafate",
+        image_url: "mafate.jpg",
+        description: "Superbe randonnée à Mafate",
+        price: 40,
+        location: "Mafate",
+        createdBy: {
+          lastname: "Doe",
+          firstname: "John",
+          email: "john.doe@gmail.com",
+          avatar: "",
+        },
+        cloudinary_public_id: "",
+        id: "",
+        userId: "",
+      } as Activity,
+      pathname: "",
+      key: "key",
+      hash: "",
+      search: "",
+    });
+
+    render(<ActivityDetails />);
+
+    const title = screen.getByRole("heading", { name: /randonnée à mafate/i });
+    const hikeImage = screen.getByAltText(/randonnée/i);
+    const description = screen.getByText(/superbe randonnée à mafate/i);
+    const userFullName = screen.getByText(/doe john/i);
+    const emptyAvatarIcon = screen.getByTestId("emptyAvatarIcon");
+    const price = screen.getByText(/40 €/i);
+    const location = screen.getByText("Mafate");
+    const mailToLink = screen.getByRole("link");
+
+    expect(title).toBeInTheDocument();
+    expect(hikeImage).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(userFullName).toBeInTheDocument();
+    expect(emptyAvatarIcon).toBeInTheDocument();
+    expect(price).toBeInTheDocument();
+    expect(location).toBeInTheDocument();
+    expect(mailToLink).toBeInTheDocument();
+    expect(mailToLink).toHaveAttribute("href", "mailto:john.doe@gmail.com");
+  });
+});
