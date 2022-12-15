a5b1f597b2495976bdf4cfe8af69827d017dcdbd
LucasTamaya <lucastamaya.974@gmail.com>
2022-12-15 13:36:55 +0400

    added unit test for IndividualUserNavLinks component

---
+++ tests/components/common/nav/links/IndividualUserNavLinks.test.tsx
@@ -0,0 +1,36 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+import { IndividualUserNavLinks } from "@/components/common/nav/links/IndividualUserNavLinks";
+import { RouterWrapper } from "@tests/helpers/RouterWrapper";
+
+const MockedComponent: React.FC = () => {
+  return (
+    <RouterWrapper>
+      <IndividualUserNavLinks />
+    </RouterWrapper>
+  );
+};
+
+describe("IndividualUserNavLinks Component", () => {
+  it("should renders 3 links", () => {
+    render(<MockedComponent />);
+
+    const linkOne = screen.getByRole("link", {
+      name: /activités du moment/i,
+    });
+    const linkTwo = screen.getByRole("link", {
+      name: /nos experts du terrain/i,
+    });
+    const linkThree = screen.getByRole("link", {
+      name: /programmer ma sortie/i,
+    });
+
+    expect(linkOne).toBeInTheDocument();
+    expect(linkOne).toHaveAttribute("href", "/activites-du-moment");
+    expect(linkTwo).toBeInTheDocument();
+    expect(linkTwo).toHaveAttribute("href", "/nos-experts-du-terrain");
+    expect(linkThree).toBeInTheDocument();
+    expect(linkThree).toHaveAttribute("href", "/programmer-ma-sortie");
+  });
+});
