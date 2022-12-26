3de335205c570d09b591fc68dc42af12d1acf929
LucasTamaya <lucastamaya.974@gmail.com>
2022-12-26 18:55:40 +0400

    updated UI for ExpertsField screen

--- src/screens/ExpertsField.tsx
+++ src/screens/ExpertsField.tsx
@@ -32,7 +32,7 @@ export const ExpertsField: React.FC = () => {
       ) : null}
 
       {data ? (
-        <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-3  lg:grid-cols-4 gap-10 px-5 pb-5">
+        <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-10 px-5 pb-5">
           {data.map(({ ...props }) => (
             <UserProviderCard {...props} key={props.id} />
           ))}
